34ca2d53e86d4279a62cf0d9e519622b17a6bdcb
carldelacruz <carl_delacruz@dlsu.edu.ph>
2022-02-14 21:37:37 +0800

    mysql dependency

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,59 @@
 
 
 const controller = {
-  /**
-   * This function renders the home page
-   * @param req - the incoming request containing either the query or body
-   * @param res - the result to be sent out after processing the request
-   */
-  getHome: (req, res) => {
-    res.render('home')
-  }
+
+  getHome: (req,res) =>{
+    const data = {
+      styles: ['style', 'sidebar'],
+      scripts: ['admin-product-data-table', 'sidebar', 'admin-toast','admin-product-modal'],
+      title: "Products Page"
+    }
+
+    res.render('home',data);
+    
+
+    // if (db.getAuth.currentUser != null) {
+    //   const email = db.getAuth.currentUser.email
+    //   db.getAll('Admin', function (adminresult) {
+    //     let flag = false
+    //     if (adminresult !== null) {
+    //       let i = 0
+    //       while (!flag && (i < adminresult.length)) {
+    //         if (email === adminresult[i].customerEmail) {
+    //           flag = true
+    //         }
+    //         i++
+    //       }
+    //       if (flag) {
+    //         db.getAll('Products', function (result) {
+    //           let products = []
+    //           result.forEach((element) => {
+    //             let item = {
+    //               productName: element.productName,
+    //               productImages: element.productImages[0],
+    //               productCategory: element.productCategory,
+    //               productPrice: element.productPrice,
+    //               productStock: element.productStock,
+    //               productDesc: element.productDesc,
+    //               productBrand: element.productBrand,
+    //               productId: element.productId,
+    //               productDiscounted: element.productDiscounted,
+    //               productDisprice: element.productDisprice
+    //             };
+    //             products.push(item)
+    //           })
+    //           data.products = products;
+    //           res.render('home',data);
+    //         });
+    //       } else {
+    //         res.redirect('/error');
+    //       }
+    //     }
+    //   })
+    // } else {
+    //   res.redirect('/error')
+    // }
+  },
 }
 
 module.exports = controller
